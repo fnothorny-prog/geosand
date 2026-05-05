7103b28dfaa3d51a9e65022c2385e062ea3ff56f
kiangonzales60-cloud <kiangonzales60@gmail.com>
2026-05-05 09:14:42 +0800

    Add Firebase URL to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -25,6 +25,8 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:5173',
+        'https://geosand-e04c1.web.app',
+        'https://geosand-e04c1.firebaseapp.com',
     ],
 
     'allowed_origins_patterns' => [],
