4a4c3ea333afab1dc06fb07e85a9950b20047414
kiangonzales60-cloud <kiangonzales60@gmail.com>
2026-05-05 08:58:22 +0800

    Fix environment variable substitution

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Export environment variables for envsubst
+export DB_HOST DB_PORT DB_DATABASE DB_USERNAME DB_PASSWORD
+
 # Replace environment variables in .env file
 envsubst < .env > .env.tmp && mv .env.tmp .env
 
